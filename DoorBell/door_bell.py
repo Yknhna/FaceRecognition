0138a89d3ad4e331a6456fa9ee6b2ec13208262a
yukinohana <sminjae1027@gmail.com>
2025-06-03 23:39:45 -0400

    add white list encode_1

--- DoorBell/door_bell.py
+++ DoorBell/door_bell.py
@@ -1,17 +1,27 @@
 import cv2
 import numpy as np
 import face_recognition
+import os
 
-white_list = []
+white_list_path = 'White_List' # path to the white list file
+white_list_images = [] # list to store white list images
+white_list_names = [] # list to store white list names
+white_list = os.listdir(white_list_path) # get all images in the white list directory
 
-def add_to_white_list(image_path):
+for person in white_list:
+    img = cv2.imread(f'{white_list_path}/{person}')
+    white_list_images.append(img)
+    white_list_names.append(os.path.splitext(person)[0])  # Store names without file extension
+
+
+def add_to_white_list(person):
     global white_list
-    img = face_recognition.load_image_file(image_path)
+    img = face_recognition.load_image_file(person)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     face_encodings = face_recognition.face_encodings(img)
 
     if face_encodings:
         white_list.append(face_encodings[0])
-        print(f"Added {image_path} to white list.")
+        print(f"Added {person} to white list.")
     else:
-        print(f"No face found in {image_path}.")
+        print(f"No face found in {person}.")
