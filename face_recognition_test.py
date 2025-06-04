78c196505983e5d336d3be77f67e31e017884048
yukinohana <sminjae1027@gmail.com>
2025-06-03 23:13:21 -0400

    Update face_recognition_test.py

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import face_recognition
 
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="pkg_resources")
+
 
 img_elon = face_recognition.load_image_file('images/elon_musk1.jpg')
 img_elon = cv2.cvtColor(img_elon, cv2.COLOR_BGR2RGB)
