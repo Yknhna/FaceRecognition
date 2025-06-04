7163cabeaf99415825424f7563f4fd8fb7fce599
daechan0615 <d.eom@mail.utoronto.ca>
2025-06-03 22:55:06 -0400

    testing Face Recognition pkg

---
+++ face_recognition_test.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import face_recognition
+
+
+img_elon = face_recognition.load_image_file('images/elon_musk1.jpg')
+img_elon = cv2.cvtColor(img_elon, cv2.COLOR_BGR2RGB)
+
+img_test_elon = face_recognition.load_image_file('images/elon_musk2.jpg')
+img_test_elon = cv2.cvtColor(img_test_elon, cv2.COLOR_BGR2RGB)
+
+cv2.imshow('Elon Musk', img_elon)
+cv2.imshow('Elon Musk Test', img_test_elon)
+cv2.waitKey(0)
