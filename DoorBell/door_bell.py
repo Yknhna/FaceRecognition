f62d033ba57ea025ae3a997184474d38deadc12d
yukinohana <sminjae1027@gmail.com>
2025-06-04 00:16:48 -0400

    Update door_bell.py

--- DoorBell/door_bell.py
+++ DoorBell/door_bell.py
@@ -4,15 +4,16 @@ import face_recognition
 import os
 
 white_list_path = 'White_List' # path to the white list file
-white_list_images = [] # list to store white list images
-white_list_names = [] # list to store white list names
-white_list = os.listdir(white_list_path) # get all images in the white list directory
+white_list = []
+white_list_images = os.listdir(white_list_path) # get all images in the white list directory
 
-for person in white_list:
+
+# whitelist : [elon must.jpg, elon_musk2.jpg]
+# splittext -> elon_must
+
+for person in white_list_images:
     img = cv2.imread(f'{white_list_path}/{person}')
-    white_list_images.append(img)
-    white_list_names.append(os.path.splitext(person)[0])  # Store names without file extension
-
+    white_list.append((img, os.path.splitext(person)[0]))
 
 def encode_faces(white_list_images):
     encode_faces = []
@@ -27,13 +28,26 @@ def encode_faces(white_list_images):
             encode_faces.append(None)  # Append None if no face is found
 
 def add_to_white_list(person):
-    global white_list
+    global white_list_images
     img = face_recognition.load_image_file(person)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     face_encodings = face_recognition.face_encodings(img)
 
     if face_encodings:
-        white_list.append(face_encodings[0])
+        white_list_images.append(face_encodings[0])
         print(f"Added {person} to white list.")
     else:
         print(f"No face found in {person}.")
+
+def run_camera():
+    captured_faces = cv2 .VideoCapture(0)
+
+    while True:
+        succ, img = captured_faces.read()
+        img_small = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+
+        face_in_frame = face_recognition.face_locations(img_small)
+        encode_frame = face_recognition.face_encodings(img_small, face_in_frame)
+
+        
