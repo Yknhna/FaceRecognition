5aff377866e00c615110098d1c5f1cf7c0da8c95
daechan0615 <d.eom@mail.utoronto.ca>
2025-06-05 19:51:10 -0400

    Added necessary methods to white_list class. Changed some directories

--- DoorBell/door_bell.py
+++ DoorBell/door_bell.py
@@ -3,34 +3,29 @@ import numpy as np
 import face_recognition
 import os
 
-white_list_path: str = 'Samples' # path to the white list file
-white_list_images: list[str] = os.listdir(white_list_path) # get all images in the white list directory
-
-white_list: list[tuple] = []
-encoded_faces: list = []
+white_list_path = 'White_List'  # path to the white list file
+white_list = []
+white_list_images = os.listdir(white_list_path)  # get all images in the white list directory
 
 # whitelist : [elon must.jpg, elon_musk2.jpg]
 # splittext -> elon_must
 
 for person in white_list_images:
-    img = face_recognition.load_image_file('Samples/donald_trump2.jpg') # 'eg. White_List/person.jpg'
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Converts BGR to RGB
+    img = cv2.imread(f'{white_list_path}/{person}')
     white_list.append((img, os.path.splitext(person)[0]))
 
 
-def encode_faces(white_lists: list) -> None:
-    """ Encodes each white listed person's face and stores them in a list, encoded_faces.
-    Returns None
-    """
-    for person in white_lists:
-        image = person[0]
-        face_encodings = face_recognition.face_encodings(image)
+def encode_faces(white_list_images):
+    encode_faces = []
+
+    for img in white_list_images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        face_encodings = face_recognition.face_encodings(img)
 
         if face_encodings:
-            encoded_faces.append(face_encodings[0])
+            encode_faces.append(face_encodings[0])
         else:
-            encoded_faces.append(None)  # Append None if no face is found
-
+            encode_faces.append(None)  # Append None if no face is found
 
 
 def add_to_white_list(person):
@@ -47,7 +42,7 @@ def add_to_white_list(person):
 
 
 def run_camera():
-    captured_faces = cv2 .VideoCapture(0)
+    captured_faces = cv2.VideoCapture(0)
 
     while True:
         succ, img = captured_faces.read()
@@ -56,10 +51,4 @@ def run_camera():
 
         face_in_frame = face_recognition.face_locations(img_small)
         encode_frame = face_recognition.face_encodings(img_small, face_in_frame)
-
-
-if __name__ == '__main__':
-    encode_faces(white_list)
-    for item in white_list:
-        print(item)
 
