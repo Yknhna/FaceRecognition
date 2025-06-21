b4fd77d46e73fd5bedde27c9adc6baf2dba4f367
yukinohana <sminjae1027@gmail.com>
2025-06-21 01:26:51 -0400

    Add test file
    Add test file, edit door bell class for accessibility and compatibility.

--- DoorBell/door_bell.py
+++ DoorBell/door_bell.py
@@ -4,64 +4,72 @@ import face_recognition
 import os
 from white_list import WhiteList
 
-white_list = None
-encoded_faces = None
-white_list_images = None
+class DoorBell:
+    def __init__(self):
+        white_list = WhiteList('DoorBell/Samples')
+        encoded_faces = white_list.get_encoded_faces()
+        white_list_images = white_list.get_white_lists()
 
-def setup_camera():
-    white_list = WhiteList('DoorBell/Samples')
-    encoded_faces = white_list.get_encoded_faces()
-    white_list_images = white_list.get_white_lists()
+    def view_white_list(self):
+        print("White List:")
 
-def run_door_bell():
-    captured_faces = cv2.VideoCapture(0)
+    def run_door_bell(self):
+        cam = cv2.VideoCapture(0)
 
-    while True:
-        succ, img = captured_faces.read()
-        img_small = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
+        if not cam.isOpened():
+            print("Error: Could not open camera.")
+            return
 
-        face_in_frame = face_recognition.face_locations(img_small)
-        encode_frame = face_recognition.face_encodings(img_small, face_in_frame)
+        while True:
+            succ, frame = cam.read()
 
-        for encode_face, face_location in zip(encode_frame, face_in_frame):
-            matches = face_recognition.compare_faces(encoded_faces, encode_face)
-            face_distance = face_recognition.face_distance(encoded_faces, encode_face)
+            if not succ:
+                print("Error: Could not read frame.")
+                break
 
-            match_index = np.argmin(face_distance)
+            # resizing the image for faster processing
+            frame_small = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+            frame_small = cv2.cvtColor(frame_small, cv2.COLOR_BGR2RGB)
 
-            if matches[match_index]:
-                name = white_list_images[match_index][1]
+            face_loc = face_recognition.face_locations(frame_small)
+            encode_frame = face_recognition.face_encodings(frame_small, face_loc)
+
+            for encode_face, face_location in zip(encode_frame, face_loc):
+                matches = face_recognition.compare_faces(encoded_faces, encode_face)
+                face_distance = face_recognition.face_distance(encoded_faces, encode_face)
+
+                match_index = np.argmin(face_distance)
+
+                if matches[match_index]:
+                    name = white_list_images[match_index][1]
+                    y1, x2, y2, x1 = face_location
+                    y1 *= 4
+                    x2 *= 4
+                    y2 *= 4
+                    x1 *= 4
+
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+                    cv2.putText(frame, name, (x1 + 6, y1 - 6), cv2.FONT_HERSHEY_PLAIN,
+                                1.5, (255, 255, 255), 2)
+
+            # if the face is not in the white list, draw a rectangle around the face and label it as "Unknown"
+            for face_location in face_loc:
                 y1, x2, y2, x1 = face_location
                 y1 *= 4
                 x2 *= 4
                 y2 *= 4
                 x1 *= 4
 
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 0), 2)
-                cv2.putText(img, name, (x1 + 6, y1 - 6), cv2.FONT_HERSHEY_PLAIN,
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.putText(frame, 'Unknown', (x1 + 6, y1 - 6), cv2.FONT_HERSHEY_PLAIN,
                             1.5, (255, 255, 255), 2)
+                
 
-        # if the face is not in the white list, draw a rectangle around the face and label it as "Unknown"
-        for face_location in face_in_frame:
-            y1, x2, y2, x1 = face_location
-            y1 *= 4
-            x2 *= 4
-            y2 *= 4
-            x1 *= 4
-
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.putText(img, 'Unknown', (x1 + 6, y1 - 6), cv2.FONT_HERSHEY_PLAIN,
-                        1.5, (255, 255, 255), 2)
+            cv2.imshow('Door Bell', frame)
             
+            if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('Door Bell', cv2.WND_PROP_VISIBLE) < 1:
+                break  
 
-        cv2.imshow('Door Bell', img)
-
-        # q is temp key to exit the camera (for testing purposes)
-        # this will be replaced with a end call button in the future
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    captured_faces.release()
-    cv2.destroyAllWindows()
+        cam.release()
+        cv2.destroyAllWindows()
 
---
+++ DoorBell/door_bell_test.py
@@ -0,0 +1,12 @@
+# temporary test file for doorbell
+import cv2
+import numpy as np
+import face_recognition
+from door_bell import DoorBell
+
+def test_Doorbell():
+    doorbell = DoorBell()
+    
+    doorbell.run_door_bell()
+    
+test_Doorbell()
